9484f53857b23d2236f7cb2a5a49bab0db6a8d3f
nicolorenzo <nicolorenzo@live.com>
2022-07-21 17:57:37 -0300

    Customizando o tema

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
+import { NativeBaseProvider } from 'native-base';
+import { THEME } from './src/styles/theme';
 import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
-  return <SignIn />;
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <SignIn />
+    </NativeBaseProvider>
+  );
 }
