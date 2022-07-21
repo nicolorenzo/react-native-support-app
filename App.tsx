d4d0dd978487b0da7e087424821d7e04cb053b86
nicolorenzo <nicolorenzo@live.com>
2022-07-21 17:57:46 -0300

    Customizando o tema

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import { NativeBaseProvider } from 'native-base';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 import { THEME } from './src/styles/theme';
 import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
     <NativeBaseProvider theme={THEME}>
       <SignIn />
